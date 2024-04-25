358da59151c55c8a277db50278d84d8bdc5634b7
Edward Viaene <ward@in4it.io>
2024-04-25 15:23:44 -0500

    reworked with controller-runtime

--- kubernetes-operator/cmd/controller/main.go
+++ kubernetes-operator/cmd/controller/main.go
@@ -2,54 +2,77 @@ package main
 
 import (
 	"context"
-	"flag"
-	"log"
+	"os"
 	"path/filepath"
 
 	in4itv1 "kubernetes-operator-demo/api/v1"
 
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
-func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+func init() {
+	utilruntime.Must(in4itv1.AddToScheme(scheme))
+}
+
+type reconciler struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx).WithValues("staticpage", req.NamespacedName)
+	log.Info("reconciling staticpage")
+	var staticPage in4itv1.StaticPage
+	err := r.Client.Get(ctx, req.NamespacedName, &staticPage)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 	if err != nil {
 		panic(err.Error())
 	}
 
-	in4itv1.AddToScheme(scheme.Scheme)
-	crdConfig := *config
-	crdConfig.ContentConfig.GroupVersion = &schema.GroupVersion{Group: in4itv1.GroupName, Version: in4itv1.GroupVersion}
-	crdConfig.APIPath = "/apis"
-	crdConfig.NegotiatedSerializer = serializer.NewCodecFactory(scheme.Scheme)
-	crdConfig.UserAgent = rest.DefaultKubernetesUserAgent()
+	ctrl.SetLogger(zap.New())
 
-	client, err := rest.UnversionedRESTClientFor(&crdConfig)
+	mgr, err := ctrl.NewManager(config, ctrl.Options{
+		Scheme: scheme,
+	})
 	if err != nil {
-		panic(err)
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
 	}
 
-	result := in4itv1.StaticPageList{}
-	err = client.
-		Get().
-		Resource("staticpages").
-		Do(context.Background()).
-		Into(&result)
+	err = ctrl.NewControllerManagedBy(mgr).
+		For(&in4itv1.StaticPage{}).
+		Complete(&reconciler{
+			Client: mgr.GetClient(),
+			scheme: mgr.GetScheme(),
+		})
 	if err != nil {
-		log.Fatalf("client error: %s", err)
+		setupLog.Error(err, "unable to create controller")
+		os.Exit(1)
 	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "error running manager")
+		os.Exit(1)
+	}
+
 }
